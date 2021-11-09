b004b1125957a00157c965eb87e4a1d31238c4ec
francesca <fradeluca85@gmail.com>
2021-11-09 16:50:03 +0100

    stampa numeri su html e colori css

--- js/script.js
+++ js/script.js
@@ -1,21 +1,34 @@
 // alert ('ciao')
 
-for (let i = 0; i <= 100; i++){
+
+const ul = document.querySelector('.list');
+console.log(ul);
+
+let numero;
+
+
+for (let i = 1; i <= 100; i++){
 
     // se il numero è multiplo di 3 e 5
     if((i % 3 == 0) && (i % 5 == 0)) {
+        numero = 'fizzbuzz';
         console.log(i + ' fizzbuzz');
     }
     // se il numero è multiplo di 3
     else if(i % 3 == 0) {
+        numero = 'fizz';
         console.log(i + ' fizz');
     } 
     // se il numero è multiplo di 5
     else if(i % 5 == 0) {
+        numero = 'buzz';
         console.log(i + ' buzz');
     } 
     // se il mumero non è un multiplo di 3 o 5
     else {
+        numero = 'numero';
         console.log(i);
     }
+
+    ul.innerHTML += `<li class="box ${numero}">${i}</li>`;
 }
