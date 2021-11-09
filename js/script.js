88bced2c59fb5600fdca095d2cf5f17bf4be3891
francesca <fradeluca85@gmail.com>
2021-11-09 18:26:04 +0100

    console log e css

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 // alert ('ciao')
 
+// Se il numero è un multiplo di 3 il programma scriverà Fizz, se il numero è multiiplo di 5 il programma scriverà Buzz, se il numero è multiplo sia di 3 che 5 il programma scriveràa FizzBuzz
+
+
 
 const ul = document.querySelector('.list');
-console.log(ul);
+// console.log(ul);
 
 
 
@@ -14,22 +17,22 @@ for (let i = 1; i <= 100; i++){
     // se il numero è multiplo di 3 e 5
     if((i % 3 == 0) && (i % 5 == 0)) {
         numero = 'fizzbuzz';
-        console.log(i + ' fizzbuzz');
+        // console.log(i + ' fizzbuzz');
     }
     // se il numero è multiplo di 3
     else if(i % 3 == 0) {
         numero = 'fizz';
-        console.log(i + ' fizz');
+        // console.log(i + ' fizz');
     } 
     // se il numero è multiplo di 5
     else if(i % 5 == 0) {
         numero = 'buzz';
-        console.log(i + ' buzz');
+        // console.log(i + ' buzz');
     } 
     // se il mumero non è un multiplo di 3 o 5
     else {
         numero = i;
-        console.log(i);
+        // console.log(i);
     }
 
     ul.innerHTML += `<li class="box ${numero}">${numero}</li>`;
